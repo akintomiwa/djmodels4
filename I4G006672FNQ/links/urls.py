81501530aa1f5461039ea29691101efaacc4d6a0
Isaac Akin-Onigbinde <aakinonigbinde@gmail.com>
2022-07-17 18:18:37 +0100

    equalize to template

---
+++ I4G006672FNQ/links/urls.py
@@ -0,0 +1,16 @@
+# from django.urls import @app.get('/path_name')
+# def method_name():
+#     pass
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name="links"
+
+urlpatterns = {
+    path("create/", views.PostCreateApi.as_view(), name="api_create"),
+    path("update/<int:pk>", views.PostUpdateApi.as_view(), name="api_update"),
+    path("delete/<int:pk>", views.PostDeleteApi.as_view(), name="api_delete"),
+    path("", views.PostListApi.as_view(), name = "api_list"),
+    path("api/links/", include("links.urls")),
+}
---
+++ I4G006672FNQ/links/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from .models import Link
+from .serializers import LinkSerializer
+
+
+# class PostViewSet(viewsets.ModelViewset):
+#     queryset = Link.objects.all()
+#     serializer_class = LinkSerializer
+
+
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import authentication, permissions
+# from django.contrib.auth.models import User
+
+# class ListUsers(APIView):
+#     """
+#     View to list all users in the system.
+
+#     * Requires token authentication.
+#     * Only admin users are able to access this view.
+#     """
+#     authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+
+#     def get(self, request, format=None):
+#         """
+#         Return a list of all users.
+#         """
+#         usernames = [user.username for user in User.objects.all()]
+#         return Response(usernames)
+
+# Create your views here.
+
+
+from django.contrib.auth.models import User
+from myapp.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
